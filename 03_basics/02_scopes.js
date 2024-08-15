97c25cff7f5b33ab4af9122657e8add4e991aa29
vivek4nag <vivek4nag@gmail.com>
2024-08-15 17:24:02 +0530

    Add files via upload

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// scope bascially koi variable ek programme ke kis part se accessible ho skta hai 
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a); // it will not print bcz let ka scope is within that { } only
+//console.log(b);  //same b ka bhi scope utna hi hai
+console.log(c); // ye print ho jayga
+
+//now block scope & global scope
+
+let x = 300 // yahn x is in global scope
+if (true){
+    let x = 10
+    const y = 400
+    console.log("inner value: ", x); // yahn x is in block scope
+    
+}
+console.log(x);
+
+
