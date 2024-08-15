081e095e266d2aa8eb1f1dc34da7cb4508b45450
viek4nag <vivek4nag@gmail.com>
2024-08-15 21:00:05 +0530

    scopes further depth

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -21,3 +21,50 @@ if (true){
 console.log(x);
 
 
+//nested scope
+function one(){
+    const username = "Vivek"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); // ReferenceError: website is not defined. scope of website is only within the function two & hence can't be accesssed outisde
+
+    two() // jab ye execute hoga to username print ho jayga i.e. function cal access bahar ka variable
+}
+one()
+
+
+//similar nested scope in if else
+if(true){
+    const username = "Vivek"
+    if(username === "Vivek"){
+        const website = " Youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website); // ReferenceError: website is not defined bcz website is defined inside the nested if block & can't be accesses outside
+    
+}
+// console.log(username); // similarly username can't be accesses bcz it is defined inside if block & it's scope is only till that only
+
+
+
+//  ++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+function addone(num){ // ye ek simple function hai 
+    return num + 1
+}
+addone(5)
+
+//niche wale ko kabhi kabhi expression bulate hai.. it is like variable. in JS variable can hold anything
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
+
+
+
+
